f331b0c9704c69849db8d73a3cbc790782a70c8a
KimMingeon0625 <machine.kmg@gmail.com>
2021-09-12 23:58:33 +0900

    entity base by mingeon

--- src/main/java/domain/entity/AnnualFee.java
+++ src/main/java/domain/entity/AnnualFee.java
@@ -1,4 +1,22 @@
 package domain.entity;
 
+
+import javax.persistence.*;
+
+@Entity
 public class AnnualFee {
+
+    @Id
+    @GeneratedValue
+    @Column(name="annual_fee_seq")
+    private Long annualFeeSeq;
+
+    @JoinColumn(name="card_info_seq")
+    @ManyToOne
+    private CardInfo cardInfo;
+
+    private String cardBrand;
+
+    private long fee;
+
 }
--- src/main/java/domain/entity/Benefit.java
+++ src/main/java/domain/entity/Benefit.java
@@ -1,4 +1,28 @@
 package domain.entity;
 
+import javax.persistence.*;
+
+@Entity
 public class Benefit {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "benefit_seq")
+    private long benefitSeq;
+
+    @ManyToOne
+    @JoinColumn(name = "card_info_seq")
+    private CardInfo cardInfo;
+
+    @Enumerated
+    private BenefitType benefitType;
+
+    private String benefitCont;
+
+    @OneToMany
+    @JoinColumn(name = "benefitCategory")
+    private BenegitCategory benegitCategory;
+
+    private String performance;
+
 }
