73275aea7a88deb3f4069d2a220151095dceb6bd
KimMingeon0625 <machine.kmg@gmail.com>
2021-09-13 00:06:02 +0900

    add Entity cardInfo by mingeon

--- src/main/java/domain/entity/CardInfo.java
+++ src/main/java/domain/entity/CardInfo.java
@@ -1,4 +1,27 @@
 package domain.entity;
 
+import javax.persistence.*;
+import java.util.List;
+
 public class CardInfo {
+    @Id
+    @GeneratedValue
+    private Long cardInfoSeq;
+
+    private String CardName;
+
+    @OneToMany(mappedBy = "cardInfo")
+    private List<Benefit> benefitList;
+
+    @OneToMany(mappedBy = "AnnualFee")
+    private List<Benefit> annualfeeList;
+
+    private String companyName;
+
+    @Enumerated(EnumType.STRING)
+    private TrafficStatus trafficYn;
+
+    @Enumerated(EnumType.STRING)
+    private CreditStatus creditYn;
+
 }
