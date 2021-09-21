489834f7a11bed5d5ee55b9fa75b044cdad0c2bc
Donggun.Chung <chungdk1117@gmail.com>
2021-09-21 23:55:56 +0900

    :hammer: 2021.09.21.Tue [add UserCardSerivce]

---
+++ src/main/java/com/cardmap/service/UserCardService.java
@@ -0,0 +1,62 @@
+package com.CardMap.service;
+
+import com.CardMap.domain.entity.CardUseHist;
+import com.CardMap.domain.entity.User;
+import com.CardMap.domain.entity.UserCard;
+import com.CardMap.domain.repository.UserCardRepository;
+import com.CardMap.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserCardService {
+
+    private final UserCardRepository userCardRepository;
+    private final UserRepository userRepository;
+
+    /**
+     * 사용자 카드 등록
+     * @param userId 사용자 아이디
+     * @param cardNo 카드 번호
+     * @param cardName 카드 이름
+     * @param cvcNo CVC 번호
+     * @return 등록 카드 번호
+     */
+    @Transactional
+    public String registUserCard(String userId, String cardNo, String cardName, String cvcNo) {
+
+        // TODO user find method
+        User user = userRepository.findOne(userId);
+
+        UserCard userCard = UserCard.createUserCard(user, cardNo, cardName, cvcNo);
+        userCardRepository.registUserCard(userCard);
+
+        return userCard.getCardNo();
+    }
+
+    /**
+     * 사용자 카드 삭제
+     * @param cardNo 카드 번호
+     */
+    @Transactional
+    public void removeUserCard(String cardNo) {
+        userCardRepository.removeUserCard(cardNo);
+    }
+
+    /**
+     * 카드 사용 내역 조회
+     * @param cardNo 카드 번호
+     * @param startDate 조회 시작일
+     * @param endDate 조회 종료일
+     * @return 카드 사용 내역 목록
+     */
+    public List<CardUseHist> getCardUseHist(String cardNo, LocalDateTime startDate, LocalDateTime endDate) {
+        return userCardRepository.getCardUseHist(cardNo, startDate, endDate);
+    }
+}
