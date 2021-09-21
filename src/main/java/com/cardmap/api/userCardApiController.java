f3301be3998d192397b313ebee9cdddf46e6107d
Donggun.Chung <chungdk1117@gmail.com>
2021-09-21 21:54:34 +0900

    :hammer: 2021.09.21.Tue [add userCardApiController]

---
+++ src/main/java/com/cardmap/api/userCardApiController.java
@@ -0,0 +1,11 @@
+package com.CardMap.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class userCardApiController {
+
+    
+}
