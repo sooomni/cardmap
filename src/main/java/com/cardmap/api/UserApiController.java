96c7a27e782c4d7b39833228ead3f64632ba2f36
KimMingeon0625 <machine.kmg@gmail.com>
2021-10-04 19:56:49 +0900

    userController mapping URI 수정

--- src/main/java/com/cardmap/api/UserApiController.java
+++ src/main/java/com/cardmap/api/UserApiController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/")
+@RequestMapping("/api/users/")
 public class UserApiController {
 
     private final UserService userService;
@@ -22,7 +22,7 @@ public class UserApiController {
      * @param request
      * @return
      */
-    @PostMapping("/users")
+    @PostMapping("")
     public void registUser(@RequestBody @Valid RegistUserRequest request){
         userService.registUser(request);
         return ;
@@ -33,7 +33,7 @@ public class UserApiController {
      * @param userId
      * @return
      */
-    @GetMapping("/users/{userId}")
+    @GetMapping("/{userId}")
     public UserDto getUser(@PathVariable("userUd") String userId){
         return userService.getUser(userId);
     }
@@ -42,8 +42,8 @@ public class UserApiController {
      * 회원 정보 수정
      * @param request
      */
-    @PutMapping("/users/{userId}")
-    public void updateUser(@RequestBody @Valid UpdateUserRequest request){
+    @PutMapping("/{userId}")
+    public void updateUser(@PathVariable("userUd") String userId, @RequestBody @Valid UpdateUserRequest request){
         userService.updateUser(request);
         return;
     }
@@ -52,8 +52,8 @@ public class UserApiController {
      * 회원 탈퇴
      * @param request
      */
-    @PutMapping("/users/delete")
-    public void RemoveUser(@PathVariable("userId") DeleteUserRequest request){
+    @PutMapping("/{userId}/delete")
+    public void RemoveUser(@PathVariable("userId") String userId, @RequestBody @Valid  DeleteUserRequest request){
         userService.deleteUser(request);
         return;
     }
@@ -62,7 +62,7 @@ public class UserApiController {
      * 아이디 찾기
      * @param request
      */
-    @PostMapping("/users/find-id")
+    @PostMapping("/find-id")
     public void findUserId(@RequestBody @Valid FindUserIdRequest request){
         userService.findUserId(request);
         return;
@@ -72,7 +72,7 @@ public class UserApiController {
      * 비밀번호 찾기
      * @param request
      */
-    @PostMapping("/users/find-pw")
+    @PostMapping("/find-pw")
     public void findUserPassword(@RequestBody @Valid FindUserPasswordRequest request){
         userService.findUserPassword(request);
         return;
@@ -82,7 +82,7 @@ public class UserApiController {
      * 즐겨찾기 등록
      * @param request
      */
-    @PostMapping("/users/{userId}/bookmarks")
+    @PostMapping("/{userId}/bookmarks")
     public void registBookmark(@RequestBody @Valid RegistBookmarkRequest request){
         userService.registBookmark(request);
         return;
@@ -93,7 +93,7 @@ public class UserApiController {
      * @param userId
      * @param placeId
      */
-    @DeleteMapping("/users/{userId}/bookmarks/{placeId}")
+    @DeleteMapping("/{userId}/bookmarks/{placeId}")
     public void deleteBookmark(
             @PathVariable("userId") String userId,
             @PathVariable("plcaeId") String placeId){
@@ -105,7 +105,7 @@ public class UserApiController {
      * 즐겨찾기 목록 조회
      * @param userId
      */
-    @GetMapping("/users/{userId}/bookmarks")
+    @GetMapping("/{userId}/bookmarks")
     public List<BookmarkDto> getBookmarkList(@PathVariable("userId") String userId){
         return userService.getBookmarks(userId);
     }
